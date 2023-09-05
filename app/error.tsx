e42559c4c73c5adc95c87a7a77a84812372de5bc
JuricLeo <ljurickc@gmail.com>
2023-09-05 10:47:20 +0200

    initial commit

---
+++ app/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import EmptyState from "./components/EmptyState";
+
+interface ErrorProps {
+  error: Error;
+}
+
+const Error: React.FC<ErrorProps> = ({ error }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return <EmptyState title="Uh oh" subtitle="Something went wrong!" />;
+};
+
+export default Error;
